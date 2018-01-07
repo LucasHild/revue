626ffdd1dcb9a6cd8e9c5aa4620788ce75d626bd
Lucas Hild <contact@lucas-hild.de>
2018-01-07 16:27:05 +0100

    Add comments

---
+++ client/src/components/Comment.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div class="comment">
+    <h4>by <a href="">{{ user.username }}</a> on {{ created.date }} at {{ created.time }}</a></h4>
+    <p v-text="content"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comment',
+  props: [
+    'user',
+    'created',
+    'content'
+  ],
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="css">
+  .comment {
+    background: #f2f2f2;
+    padding: 20px 10px;
+    margin-bottom: 20px;
+  }
+
+  h4 {
+    margin: 5px 0;
+  }
+
+  p {
+    margin: 5px 0;
+  }
+</style>
--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -6,12 +6,19 @@
       <p v-html="content"></p>
       <i class="post-id">ID {{ id }}</i>
     </div>
+    <div class="container">
+      <h2>Comments</h2>
+      <Comment v-for="comment in comments" :key="comment.id" :user="comment.user" :created="comment.created" :content="comment.content"></Comment>
+    </div>
   </div>
 </template>
 
 <script>
+import Comment from '@/components/Comment'
+
 export default {
   name: 'post',
+  components: { Comment },
   data() {
     return {
       id: this.$route.params.id,
@@ -21,11 +28,13 @@ export default {
       content: 'Lorem ipsum dolor sit <strong>amet</strong>, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos e',
       comments: [
         {
+          id: 11,
           user: { 'username': 'Lanseuo' },
           created: { date: '7 January 2018', time: '16:03' },
           content: 'Lorem ipsum dolor sit <strong>amet</strong>, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos e',
         },
         {
+          id: 12,
           user: { 'username': 'Lanseuo' },
           created: { date: '7 January 2018', time: '16:03' },
           content: 'Lorem ipsum dolor sit <strong>amet</strong>, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos e',
