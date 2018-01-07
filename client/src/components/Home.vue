4f199397f13256ce25d768af7b289881eec36f27
Lucas Hild <contact@lucas-hild.de>
2018-01-07 16:20:27 +0100

    Add post

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,11 +1,18 @@
 <template lang="html">
-  <div class="home">
-    <h1>Home</h1>
+  <div class="home container">
+    <h1>Recenst Posts</h1>
+    <PostPreview img="http://fillmurray.com/225/150" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consequuntur voluptate quis in qui delectus adipisci quam, quisquam vero sequi eveniet accusamus, labore animi, porro soluta tenetur quos, eos aliquid. Assumenda iure quo vel obcaecati reprehenderit qui exercitationem. Architecto, alias numquam illo debitis quos consequatur, voluptas officia vero ab quibusdam consectetur amet quisquam adipisci laboriosam nisi. Facere pariatur est mollitia maxime repudiandae temporibus, omnis distinctio unde ipsa neque non illo cum, optio, consectetur dicta voluptatibus quam corrupti id explicabo doloribus modi exercitationem? Optio, fugiat. Inventore, provident ipsum voluptatem accusamus nobis accusantium repudiandae perferendis expedita ad, rerum doloremque, vel ea consequuntur." id="4">Hello World!</PostPreview>
+    <PostPreview img="http://fillmurray.com/225/150" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consequuntur voluptate quis in qui delectus adipisci quam, quisquam vero sequi eveniet accusamus, labore animi, porro soluta tenetur quos, eos aliquid. Assumenda iure quo vel obcaecati reprehenderit qui exercitationem. Architecto, alias numquam illo debitis quos consequatur, voluptas officia vero ab quibusdam consectetur amet quisquam adipisci laboriosam nisi. Facere pariatur est mollitia maxime repudiandae temporibus, omnis distinctio unde ipsa neque non illo cum, optio, consectetur dicta voluptatibus quam corrupti id explicabo doloribus modi exercitationem? Optio, fugiat. Inventore, provident ipsum voluptatem accusamus nobis accusantium repudiandae perferendis expedita ad, rerum doloremque, vel ea consequuntur." id="5">Hello World!</PostPreview>
+    <PostPreview img="http://fillmurray.com/225/150" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consequuntur voluptate quis in qui delectus adipisci quam, quisquam vero sequi eveniet accusamus, labore animi, porro soluta tenetur quos, eos aliquid. Assumenda iure quo vel obcaecati reprehenderit qui exercitationem. Architecto, alias numquam illo debitis quos consequatur, voluptas officia vero ab quibusdam consectetur amet quisquam adipisci laboriosam nisi. Facere pariatur est mollitia maxime repudiandae temporibus, omnis distinctio unde ipsa neque non illo cum, optio, consectetur dicta voluptatibus quam corrupti id explicabo doloribus modi exercitationem? Optio, fugiat. Inventore, provident ipsum voluptatem accusamus nobis accusantium repudiandae perferendis expedita ad, rerum doloremque, vel ea consequuntur." id="6">Hello World!</PostPreview>
   </div>
 </template>
 
 <script>
+import PostPreview from '@/components/PostPreview'
+
 export default {
+  name: 'home',
+  components: { PostPreview }
 }
 </script>
 
--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -1,6 +1,11 @@
 <template lang="html">
-  <div class="post container">
-    <h1>Post ({{ id }})</h1>
+  <div class="post">
+    <div class="body container">
+      <h1>{{ title }}</h1>
+      <h3>by <a href="#">{{ user.username }}</a> on {{ created.date }} at {{ created.time }}</h3>
+      <p v-html="content"></p>
+      <i class="post-id">ID {{ id }}</i>
+    </div>
   </div>
 </template>
 
@@ -9,11 +14,44 @@ export default {
   name: 'post',
   data() {
     return {
-      id: this.$route.params.id
+      id: this.$route.params.id,
+      title: 'Hello World',
+      user: { 'username': 'Lanseuo' },
+      created: { date: '7 January 2018', time: '16:03' },
+      content: 'Lorem ipsum dolor sit <strong>amet</strong>, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos e',
+      comments: [
+        {
+          user: { 'username': 'Lanseuo' },
+          created: { date: '7 January 2018', time: '16:03' },
+          content: 'Lorem ipsum dolor sit <strong>amet</strong>, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos e',
+        },
+        {
+          user: { 'username': 'Lanseuo' },
+          created: { date: '7 January 2018', time: '16:03' },
+          content: 'Lorem ipsum dolor sit <strong>amet</strong>, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos e',
+        }
+      ]
     }
   }
 }
 </script>
 
-<style lang="css">
+<style scoped lang="css">
+  .body {
+    background: #eeeeee;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  h3 {
+    margin: 0;
+    font-weight: 400;
+  }
+
+  .post-id {
+    text-align: right;
+    display: block;
+  }
 </style>
