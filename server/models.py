a49f5ebd260661414d02aac079b72b89cfeac056
Lucas Hild <contact@lucas-hild.de>
2018-01-14 15:05:52 +0100

    Add db models to server

---
+++ server/models.py
@@ -0,0 +1,27 @@
+import datetime
+
+from mongoengine import *
+
+
+connect("revue")
+
+
+class User(Document):
+    email = EmailField(required=True, unique=True)
+    username = StringField(max_length=50, required=True, unique=True)
+    password = StringField(required=True)
+    created = DateTimeField(required=True, default=datetime.datetime.now())
+
+
+class Comment(EmbeddedDocument):
+    content = StringField(max_length=5000)
+    user = ReferenceField(User)
+    created = DateTimeField(required=True, default=datetime.datetime.now())
+
+
+class Post(Document):
+    title = StringField(max_length=120, required=True)
+    user = ReferenceField(User, reverse_delete_rule=CASCADE)
+    content = StringField(max_length=5000)
+    comments = ListField(EmbeddedDocumentField(Comment))
+    created = DateTimeField(required=True, default=datetime.datetime.now())
