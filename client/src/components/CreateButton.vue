c5e49b130f76a30de9a80eecae617f7edafb2220
Lucas Hild <contact@lucas-hild.de>
2018-01-19 21:06:36 +0100

    Add user authentication

--- client/src/components/CreateButton.vue
+++ client/src/components/CreateButton.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div @click="$router.push({name: 'CreatePost'})" class="create-button">
+  <div v-show="$store.state.isUserLoggedIn" @click="$router.push({name: 'CreatePost'})" class="create-button">
       <svg fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
         <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
         <path d="M0 0h24v24H0z" fill="none"/>
--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
+import store from '@/store/store'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'http://localhost:5000/api',
+    headers: {
+      Authorization: `JWT ${store.state.token}`
+    }
   })
 }
